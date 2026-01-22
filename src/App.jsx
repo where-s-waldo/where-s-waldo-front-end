4e488d0a5180405e195c4e152f02f36245fd0f7f
Bob <andrew@risertech.com>
2026-01-22 10:42:33 -0700

    added fake db for devlopment added waldo maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './css/globle.css'
+import './css/header.css'
+import { Link } from 'react-router-dom'
+import map1 from './assets/waldo-maps/map1.jpg'
+import map2 from './assets/waldo-maps/map2.jpg'
+import map3 from './assets/waldo-maps/map3.jpg'
+import map4 from './assets/waldo-maps/map4.jpg'
+import map5 from './assets/waldo-maps/map5.jpg'
+import map6 from './assets/waldo-maps/map6.jpg'
+import map7 from './assets/waldo-maps/map7.jpg'
+import map8 from './assets/waldo-maps/map8.jpg'
+
+const waldoMaps = [
+  { id: 1, src: map1, description: 'Find Waldo in the busy street scene.' },
+  { id: 2, src: map2, description: 'Find Waldo in the busy street scene.' },
+  { id: 3, src: map3, description: 'Find Waldo in the busy street scene.' },
+  { id: 4, src: map4, description: 'Find Waldo in the busy street scene.' },
+  { id: 5, src: map5, description: 'Find Waldo in the busy street scene.' },
+  { id: 6, src: map6, description: 'Find Waldo in the busy street scene.' },
+  { id: 7, src: map7, description: 'Find Waldo in the busy street scene.' },
+  { id: 8, src: map8, description: 'Find Waldo in the busy street scene.' },
+]
+
+const Map = ({ map }) => {
+  return (
+    <div className='map'>
+      <img src={map.src} alt={`Waldo Map ${map.id}`} width="300" />
+      <div>{map.description}</div>
+      <Link to={`/maps/${map.id}`}>go to map</Link>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Where's Waldo?</h1>
+      <div>Links</div>
+    </header>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className='waldoMaps'>
+        {waldoMaps.map((map) => (
+          <Map key={map.id} map={map} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
